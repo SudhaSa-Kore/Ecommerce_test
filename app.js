0f95a8d27538ff287f9d018d22ca16f4ba86aa84
Kstream 001 <kstream001@gmail.com>
2016-08-24 17:00:15 +0530

    doctorly call added

--- app.js
+++ app.js
@@ -36,7 +36,71 @@ app.get('/searchResult/:actionName',function(req,res){
 }); 
 
 
+app.get('/doctorsAvailablity',function(req,res){
+	res.writeHead(200,{'Content-Type':'application/json'});
+	res.end(JSON.stringify({
+    "Doctors Availablity": [
+        {
+            "fromDate": "26/08/2016",
+            "toDate": "30/08/2016",
+            "times": [
+                {
+                    "fromTime": "09:00",
+                    "toTime": "12:00"
+                },
+                {
+                    "fromTime": "14:00",
+                    "toTime": "16:00"
+                }
+            ]
+        },
+        {
+            "fromDate": "09/09/2016",
+            "toDate": "13/09/2016",
+            "times": [
+                {
+                    "fromTime": "08:30",
+                    "toTime": "10:30"
+                },
+                {
+                    "fromTime": "15:00",
+                    "toTime": "18:00"
+                }
+             ]
+        },
 
+        {
+            "fromDate": "23/10/2016",
+            "toDate": "27/10/2016",
+            "times": [
+                {
+                    "fromTime": "10:00",
+                    "toTime": "13:00"
+                },
+                {
+                    "fromTime": "16:00",
+                    "toTime": "18:00"
+            
+                }
+              ]
+        },
+        {
+            "fromDate": "10/11/2016",
+            "toDate": "15/11/2016",
+            "times": [
+                {
+                    "fromTime": "09:30",
+                    "toTime": "12:30"
+                },
+                {
+                    "fromTime": "14:00",
+                    "toTime": "17:00"
+               }
+               ]
+         }
+]
+}));
+})
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
